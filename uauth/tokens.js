a2bf62d2762dbf65104cd62320f7c28e9b8dc70b
BinBashBanana <51469593+BinBashBanana@users.noreply.github.com>
2021-04-24 22:14:16 -0700

    uauth

---
+++ uauth/tokens.js
@@ -0,0 +1,11 @@
+// Google Drive
+var googleProjectId = "hallowed-digit-311307";
+var googleApiKey = "AIzaSyAUz-Um6_rcR8X1vuX1O0ywC-q6pYZFt_c";
+var googleOauthClientId = "478810195063-are80pqls62akva4kbe6phavidr00mna.apps.googleusercontent.com";
+// If it asks you, use this scope: https://www.googleapis.com/auth/drive.file
+
+// Dropbox
+var dropboxAppKey = "2556z2wtdog5rop";
+
+// OneDrive
+var onedriveClientId = "31614988-7150-44ca-b17f-d18e3ea1d3af";
