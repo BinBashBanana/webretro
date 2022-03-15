fa9c5092480d9be8c28112e1782000a634b1368b
BinBashBanana <51469593+BinBashBanana@users.noreply.github.com>
2022-03-14 19:36:12 -0700

    uauth - new GIS API

--- uauth/tokens.js
+++ uauth/tokens.js
@@ -2,7 +2,7 @@
 var googleProjectId = "hallowed-digit-311307";
 var googleApiKey = "AIzaSyDuKbs32MBOZw87EJi0dsPVNHx6D4_QTOQ";
 var googleOauthClientId = "478810195063-c83gm38hf14o08gg4oqggql7pp9dqbdl.apps.googleusercontent.com";
-// If it asks you, use this scope: https://www.googleapis.com/auth/drive.readonly
+// scope: https://www.googleapis.com/auth/drive.readonly
 
 // Dropbox
 var dropboxAppKey = "2556z2wtdog5rop";
